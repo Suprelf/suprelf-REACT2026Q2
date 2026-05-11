cb0a260a1975c6e0de9b7d2799be13624689f9c5
suprelf <mh.suprelf@gmail.com>
2026-05-11 22:52:04 +0300

    feat: implement itemTable tests

---
+++ src/components/itemTable/itemTable.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { mockData } from '../../test-utils/tableCorrectDataMock';
+
+import ItemTable from './itemTable';
+
+describe('ItemTable', () => {
+
+  it('render headers', () => {
+    render(<ItemTable listData={mockData} />);
+
+    expect(screen.getByText('Name')).toBeInTheDocument();
+    expect(screen.getByText('Description')).toBeInTheDocument();
+  });
+
+  it('render list items with correct capitalized name and correct description value', () => {
+    render(<ItemTable listData={mockData} />);
+
+    expect(screen.getByText('Pikachu')).toBeInTheDocument();
+    expect(screen.getByText('Bulbasaur')).toBeInTheDocument();
+
+    expect(screen.getByText('https://pokeapi.co/pikachu')).toBeInTheDocument();
+    expect(
+      screen.getByText('https://pokeapi.co/bulbasaur')
+    ).toBeInTheDocument();
+  });
+
+  it('render headers but no items when listData is empty', () => {
+    render(<ItemTable listData={[]} />);
+
+    expect(screen.getByText('Name')).toBeInTheDocument();
+    expect(screen.getByText('Description')).toBeInTheDocument();
+
+    expect(screen.queryByText(/pikachu/i)).not.toBeInTheDocument();
+  });
+});
