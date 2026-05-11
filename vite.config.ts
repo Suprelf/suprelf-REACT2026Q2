c9cb2c231833b63be7bd2059359b528fcbbae63d
suprelf <mh.suprelf@gmail.com>
2026-05-11 22:28:59 +0300

    init: setup project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { configDefaults } from 'vitest/config';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+
+      exclude: [
+        ...configDefaults.exclude,
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts',
+      ],
+
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
+  },
+});
