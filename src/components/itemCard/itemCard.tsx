bcec08163ab345d7c7d6a052ccfe44423ffa973c
suprelf <mh.suprelf@gmail.com>
2026-05-24 12:44:54 +0300

    feat: implement checkbox without propagation

--- src/components/itemCard/itemCard.tsx
+++ src/components/itemCard/itemCard.tsx
@@ -1,4 +1,5 @@
 import './itemCard.css';
+import { useState } from 'react';
 
 import type { Pokemon } from '../../types/types';
 
@@ -8,19 +9,34 @@ type Props = {
 };
 
 const ItemCard = ({ pokemon, onSelect }: Props) => {
+  const [isSelected, setIsSelected] = useState(false);
+
   const formatName = (name: string) =>
     name.charAt(0).toUpperCase() + name.slice(1);
+
+  const toggleSelect = () => {
+    setIsSelected((prev) => !prev);
+  };
 
   return (
     <div
       className="grid-item"
       onClick={() => onSelect(pokemon)}
+      style={{ position: 'relative' }}
     >
-      <img
-        className="img-card"
-        src={pokemon.image}
-        alt={pokemon.name}
+      <input
+        type="checkbox"
+        checked={isSelected}
+        onChange={(e) => {
+          toggleSelect();
+        }}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        className="card-checkbox"
       />
+
+      <img className="img-card" src={pokemon.image} alt={pokemon.name} />
 
       <div>{formatName(pokemon.name)}</div>
     </div>
